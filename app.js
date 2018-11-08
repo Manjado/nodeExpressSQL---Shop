1f3cf29d6af780cae564c45aa49b4ba6c3f8a225
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-11-08 15:49:19 +0100

    add small express routes

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-/*
-const http = require('http');
-*/ //wywalamy to jak korzystamy z expressa i zapisu app.listen
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-
-
-// app.use to middleware, który zawsze przyjmuję trzy argumenty
-// app.use((req, res, next) => {
-//     console.log('first middleware');
-//     next(); // alows the request to continue to the next middleware in line
-// });
-/*app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});*/
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hellow from Express!</h1>')
-});
-/*
-const server = http.createServer(app);
-
-server.listen(3000);*/ // można to zastąpić app.listen(3000)
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
