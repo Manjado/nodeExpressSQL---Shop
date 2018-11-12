ac03e58b8ee07dabd1af9344bcf0695418b3c3b7
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-11-12 19:31:10 +0100

    finishing the pug template

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ const products = [];
 router.get('/add-product',(req, res, next) => {
     //res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>')
     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', { pageTitle: 'Add Product' });
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 router.post('/add-product', (req, res, next) => {
