021f78bdd284966919b90fbf6d71856e5b09828a
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-11-17 13:51:32 +0100

    adding the layout to handlebars

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs()); // initialised view engine
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+); // initialised view engine
 app.set('view engine', 'hbs');
-//app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -21,7 +27,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,18 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product',(req, res, next) => {
-    //res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render(
+        'add-product',
+        {
+            pageTitle: 'Add Product',
+            path: '/admin/add-product',
+            formsCSS: true,
+            productCSS: true,
+            activeAddProduct: true
+        });
 });
 
 router.post('/add-product', (req, res, next) => {
-    //console.log(req.body);
     products.push({ title: req.body.title })
     res.redirect('/');
 })
