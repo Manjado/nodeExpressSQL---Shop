646d645cb4ade17ae4654a38fb23e57c0be252cf
malchimowicz <michal.alchimowicz@enovatis.pl>
2018-11-29 21:48:59 +0100

    creating the shop structure

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render(
-        'add-product',
+        'admin/add-product',
         {
             pageTitle: 'Add Product',
             path: '/admin/add-product',
@@ -21,7 +21,7 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
         console.log(products, 'prod')
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
